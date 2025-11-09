38d06e4b900fdd836500336bb48514e7cce65e4a
Łukasz Porębski <lukaporebski@gmail.com>
2025-11-09 10:33:17 +0100

    Simplified user creation

--- Source/Infrastructure/Infrastructure.Endpoints/Modules/Users/UserProfile.cs
+++ Source/Infrastructure/Infrastructure.Endpoints/Modules/Users/UserProfile.cs
@@ -1,6 +1,7 @@
 using Application.Contracts.Modules.Users.Commands;
 using AutoMapper;
 using Common.Domain.ValueObjects;
+using Domain.Contracts.Modules.Users.Enums;
 using Domain.Modules.Users.Data;
 using PublishedLanguage.Modules.Users.Requests;
 
@@ -11,7 +12,8 @@ public class UserProfile : Profile
     public UserProfile()
     {
         CreateMap<CreateUserRequest, UserCreationData>()
-            .ForCtorParam(nameof(UserCreationData.Id), e => e.MapFrom(request => AggregateId.Generate()));
+            .ForCtorParam(nameof(UserCreationData.Id), e => e.MapFrom(request => AggregateId.Generate()))
+            .ForCtorParam(nameof(UserCreationData.Role), e => e.MapFrom(request => UserRole.User));
 
         CreateMap<CreateUserRequest, CreateUserCommand>()
             .ForCtorParam(nameof(CreateUserCommand.Data), e => e.MapFrom(request => request));
--- Source/PublishedLanguage/Modules/Users/Requests/CreateUserRequest.cs
+++ Source/PublishedLanguage/Modules/Users/Requests/CreateUserRequest.cs
@@ -1,9 +1,6 @@
-using Domain.Contracts.Modules.Users.Enums;
-
 namespace PublishedLanguage.Modules.Users.Requests;
 
 public record CreateUserRequest(
     string Email,
-    string Password,
-    UserRole Role
+    string Password
 );
