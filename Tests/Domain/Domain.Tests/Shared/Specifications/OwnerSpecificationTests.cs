baa4520a5d79ddf2af1693e90ac9965788b61963
Łukasz Porębski <lukaporebski@gmail.com>
2026-02-21 15:23:03 +0100

    Added test

---
+++ Tests/Domain/Domain.Tests/Shared/Specifications/OwnerSpecificationTests.cs
@@ -0,0 +1,36 @@
+﻿using Common.Domain.ValueObjects;
+using Common.TestsCore;
+using Domain.Shared.Interfaces;
+using Domain.Shared.Specifications;
+using FluentAssertions;
+using Xunit;
+
+namespace Domain.Tests.Shared.Specifications;
+
+public class OwnerSpecificationTests : BaseTest
+{
+    private readonly OwnerSpecification _specification = new();
+
+    [Fact]
+    public void IsValid_Should_ReturnTrue_When_OwnerId_Equals_UserId()
+    {
+        var id = new AggregateId(Guid.NewGuid());
+        
+        var result = _specification.IsValid(new TestObject(id, id));
+
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void IsValid_Should_ReturnFalse_When_OwnerId_DoesNotEqual_UserId()
+    {
+        var ownerId = new AggregateId(Guid.NewGuid());
+        var userId = new AggregateId(Guid.NewGuid());
+        
+        var result = _specification.IsValid(new TestObject(ownerId, userId));
+
+        result.Should().BeFalse();
+    }
+
+    private record TestObject(AggregateId OwnerId, AggregateId UserId) : IOwnerSpecification;
+}
